15121c67864fc7ca2f12f50fb70a4cfac5b77c40
Juan8bits <juan160820@gmail.com>
2020-09-16 22:33:28 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+* main - show some text with printf function
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
