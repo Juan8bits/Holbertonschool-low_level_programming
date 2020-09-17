ae0f5687f120b0f14efb71263d7afe1f52431a3d
Juan8bits <juan160820@gmail.com>
2020-09-17 23:42:41 +0000

    Print_reverse_alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+/**
+* main - Write a program that prints the lowercase alphabet
+*  in reverse, followed by a new line.
+* -You can only use the putchar function (every other function
+*  (printf, puts, etc) is forbidden)
+* -All your code should be in the main function
+* -You can only use putchar twice in your code
+* Return: 0
+*/
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter > 96; --letter)
+	{
+		putchar(letter);
+	}
+	putchar(10);
+	return (0);
+}
