4f1315fa69f54f9321f5941ac32071fe05d64752
Juan8bits <juan160820@gmail.com>
2020-09-23 00:40:24 +0000

    Print_to_98_and_add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+# include "holberton.h"
+
+/**
+* add - Funtions that add int 1 with int 2
+* @a: integer number 1.
+* @b: integer number 2.
+* Return: Sumatory of int1 and int 2.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+# include "holberton.h"
+# include <stdio.h>
+/**
+* print_to_98 - Print all natural numbers from n to 98.
+* @n: integer number input.
+* Return: numbers from n to 98.
+*/
+
+void print_to_98(int n)
+{
+      	for (; n < 98; n++)
+	{
+		printf("%d", n);
+		if (n == 98)
+		{
+			_putchar('\n');
+			break;
+		}
+		printf(", ");
+	}
+	for (; n > 98; n--)
+	{
+		printf("%d", n);
+		if (n == 98)
+		{
+			_putchar('\n');
+			break;
+		}
+		printf(", ");
+	}
+	printf("%d\n", n);
+}
