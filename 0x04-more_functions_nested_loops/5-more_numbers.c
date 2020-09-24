3d00a5b493c7ca8472d3b58a59ca5beddab4a32d
Juan8bits <juan160820@gmail.com>
2020-09-24 22:52:45 +0000

    Morenumbersfile

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+# include "holberton.h"
+
+/**
+* more_numbers - Print 10 times the numbers from 0 to 14
+*
+* Return: Numbers from 0 to 9. X10.
+*/
+
+void more_numbers(void)
+{
+	short int times, uni, i;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i < 10)
+			{
+				uni = i;
+			}
+			if (i > 9)
+			{
+				uni = i % 10;
+				_putchar('1');
+			}
+			_putchar(uni + '0');
+		}
+		_putchar(10);
+	}
+
+}
