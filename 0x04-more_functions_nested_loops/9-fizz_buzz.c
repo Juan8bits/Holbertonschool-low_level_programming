13cb2a47fcf0ac8e0df5dceb5ee74b5d2367dc03
Juan8bits <juan160820@gmail.com>
2020-09-25 04:01:57 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,16 @@ int main(void)
 		tres = i % 3;
 		cinco = i % 5;
 
-		if(tres == 0 && cinco == 0)
-			printf("FizzBuzz ");
+		if (tres == 0 && cinco == 0)
+			printf("FizzBuzz");
 		else if (cinco == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (tres == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ",i);
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
