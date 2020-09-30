275701d96773a2062e750a48ab233f07d925162c
Juan8bits <juan160820@gmail.com>
2020-09-30 00:53:08 +0000

    ñolberton

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+# include "holberton.h"
+
+/**
+* rev_string - reverses a string
+* @s: string in order
+*/
+
+void rev_string(char *s)
+{
+	short int z, g, i, aux;
+
+	z = 0;
+	g = 0;
+	while (s[z] != '\0')
+		z++;
+	z--;
+	for (i = z; i >= (z / 2); i--)
+	{
+		aux = s[g];
+		s[g] = s[i];
+		s[i] = aux;
+		g++;
+	}
+}
