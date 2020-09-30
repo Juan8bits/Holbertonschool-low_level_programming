009e1b1ca6fb2ecbe1f769b11225a0ebcaeb878a
Juan8bits <juan160820@gmail.com>
2020-09-30 04:50:53 +0000

    print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_array - print n characters od array
+ * @a: string
+ * @n: number of characters
+ */
+
+void print_array(int *a, int n)
+{
+	short int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
