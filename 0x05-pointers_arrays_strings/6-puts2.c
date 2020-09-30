700aa9c36dc3c9676f1f1362a4472bfa8f7d185b
Juan8bits <juan160820@gmail.com>
2020-09-30 02:47:44 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+# include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: Input strig of a characters
+ */
+
+void puts2(char *str)
+{
+	int leng, i;
+
+	while (str[leng] != '\0')
+		leng++;
+	for (i = 0; i <= leng - 1; i = i + 2)
+		_putchar(i + '0');
+	_putchar(10);
+}
