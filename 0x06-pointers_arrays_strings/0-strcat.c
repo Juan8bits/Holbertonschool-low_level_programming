085bd53143f173acf2db460b2b0b5af00c128868
Juan8bits <juan160820@gmail.com>
2020-10-01 19:05:54 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strcat - Function that concatenates two strings
+ * @dest: string concatenates
+ * @src: string source
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	short int i, j;
+
+	i = 0, j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+		if (dest[i] == '\0')
+		{
+			i++;
+			while (src[j] != '\0')
+			{
+				dest[i] = src[j];
+				i++;
+				j++;
+			}
+			/*dest[i] = '\0';*/
+		}
+	}
+	return (dest);
+}
