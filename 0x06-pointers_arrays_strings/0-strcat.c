ab29003022ca3baaa7799ca05a8b7675980b6f55
Juan8bits <juan160820@gmail.com>
2020-10-01 19:31:50 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,19 +13,12 @@ char *_strcat(char *dest, char *src)
 
 	i = 0, j = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-		if (dest[i] == '\0')
-		{
-			i++;
-			while (src[j] != '\0')
-			{
-				dest[i] = src[j];
-				i++;
-				j++;
-			}
-			/*dest[i] = '\0';*/
-		}
+    	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
 	return (dest);
 }
