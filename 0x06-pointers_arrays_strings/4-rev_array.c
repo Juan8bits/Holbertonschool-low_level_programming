3baa507bfd16f3196d4eb5bc15721b1841b2035f
Juan8bits <juan160820@gmail.com>
2020-10-02 01:28:51 +0000

    reverse_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 	short int i, g, aux;
 
 	g = 0;
-	for (i = n - 1 ; i  >= (n / 2); g++, i--)
+	for (i = n - 1 ; i  >= (n / 2); ++g, i--)
 	{
 		aux = a[i];
 		a[i] = a[g];
