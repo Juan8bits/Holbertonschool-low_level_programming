9ed01ec50e73ada537e764d433f42b061cbfbcc3
Juan8bits <juan160820@gmail.com>
2020-10-08 03:23:01 +0000

    is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - Funtiont that check if the number is prime
+ * @n: number to check.
+ * Return: 1 if it´s prime else 0.
+ */
+int is_prime_number(int n)
+{
+	int x;
+	x = 2;
+        return (rev_is_prime(n, x));
+}
+
+/**
+ * rev_is_prime -Increase x counter
+ * @n: number to check.
+ * @x: number to div.
+ * Return: x.
+ */
+int rev_is_prime(int n, int x)
+{
+	if ((n % x == 0 && x < n) || n < 2)
+	{
+		return (0);
+	}
+	else if (n % x == 0 && x == n)
+		return (1);
+	else
+		return (rev_is_prime(n, x + 1));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,5 +6,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int rev_is_prime(int n, int x);
 int is_palindrome(char *s);
 int rev(int n, int x);
