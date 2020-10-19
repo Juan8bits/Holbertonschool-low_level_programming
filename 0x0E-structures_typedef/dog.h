05a0351e4d7387cea21aa4af45db374c55c0926c
Juan8bits <juan160820@gmail.com>
2020-10-19 23:37:46 +0000

    Readme file and dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+/**
+ * struct dog - My first structure about dogs and owners.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ * Desciption: Struct that define type of variables.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+ 	float age;
+};
+#endif /* STRUCT_H */
