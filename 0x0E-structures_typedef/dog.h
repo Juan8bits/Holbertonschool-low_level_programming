29e46f928f4847669b5f033a7908798d6ec3dd2b
Juan8bits <juan160820@gmail.com>
2020-10-20 01:23:03 +0000

    Update dog.h. (Typedef)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: name of the owner.
  * Desciption: Struct that define type of variables.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
