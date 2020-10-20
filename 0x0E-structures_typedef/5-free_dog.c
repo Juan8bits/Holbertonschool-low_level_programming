978a523db4858bb8c0c6f6ae5b5c39a835c6a359
Juan8bits <juan160820@gmail.com>
2020-10-20 04:25:33 +0000

    First commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Function that frees dogs
+ * @d: Adress to pointer.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(name);
+		if (d->owner != NULL)
+			free(owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,17 +7,17 @@
  * @owner: name of the owner.
  * Desciption: Struct that define type of variables.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
-typedef struct dog dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
+void free_dog(dog_t *d);
 #endif /* STRUCT_H */
