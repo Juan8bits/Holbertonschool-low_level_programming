7e13c58ca5274dad43d44ba2bf4017361cd50b51
Juan8bits <juan160820@gmail.com>
2020-10-21 18:17:44 +0000

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that print a name
+ * @name: Name to print
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptrtof)(char *) = f;
+	(*ptrtof)(name);
+}
