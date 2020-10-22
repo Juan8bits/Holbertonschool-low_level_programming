a8b0fa275b9d0566c8b5ed3244c9013c3cac72b9
Juan8bits <juan160820@gmail.com>
2020-10-22 04:52:02 +0000

    First commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,49 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Functiont that add two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: integer.
+ */
+
+int op_add(int a, int b)
+{ return (a + b); }
+
+/**
+ * op_sub - Functiont that sub two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: integer.
+ */
+
+int op_sub(int a, int b)
+{ return (a - b); }
+/**
+ * op_mul- Functiont that multiply two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: integer.
+ */
+
+int op_mul(int a, int b)
+{ return (a * b); }
+
+/**
+ * op_div - Functiont that div two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: integer.
+ */
+
+int op_div(int a, int b)
+{ return (a / b); }
+/**
+ * op_mod - Functiont that mod two integers
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: integer.
+ */
+
+int op_mod(int a, int b)
+{ return (a % b); }
