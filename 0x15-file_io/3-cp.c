eae17b42bbfa99ddc941b4265a53021191b2d471
Juan8bits <juan160820@gmail.com>
2020-11-10 04:35:23 +0000

    Create 3-cp.c file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * main - Function that copies the content of one file to another.
+ * @ac: Number of arguments given.
+ * @av: Pointer to pointer with argument.
+ * Return: 0 or message error.
+ */
+int main(int ac, char **av)
+{
+int file_from, file_to, nread, nwrite, bufsize = 1024;
+char buf[1024];
+
+if (ac != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+exit(97);
+}
+file_from = open(av[1], O_RDONLY);
+nread = read(file_from, buf, bufsize);
+if (file_from < 0 || nread < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+av[1]);
+exit(98);
+}
+file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
+if (file_to < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+exit(99);
+}
+for (; nread > 0; nread = read(file_from, buf, bufsize))
+{
+nwrite = write(file_to, buf, nread);
+if (nwrite < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+exit(99);
+}
+}
+if (close(file_from) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+}
+if (close(file_to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+}
+return (0);
+}
