65d23a784d1bb2a783bb111d920106bc50418a25
Juan8bits <juan160820@gmail.com>
2021-04-21 04:18:26 +0000

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_arr(int *arr, int left, int right);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
