35e223b2600283ece7b1fafdd597c4d48fcfc939
Juan8bits <juan160820@gmail.com>
2021-04-22 03:13:26 +0000

    Add last prototype for advanced_binary_search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_arr(int *arr, int left, int right);
 int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
